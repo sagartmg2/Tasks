8c1f8b4e0be943e4903845d0b5913d3a321741f0
Sagar <dev.sagartmg@gmail.com>
2023-04-28 12:40:55 +0545

    nextjs task

---
+++ next/DispalyArrayDatas/component/SingleTodoDetail.jsx
@@ -0,0 +1,23 @@
+import Output from "/assets/todo-display.png"
+import Image from "next/image";
+
+export default function SingleTodoDetail() {
+    let title = "Learn REACT";
+    let status = false
+    let deadline = "2023-01-01"
+
+    return (
+        <div>
+            <h2>Show Single Todo // output should be as image below</h2>
+            <Image src={Output} alt="" />
+            {/* TODO: code here */}
+
+            {/* <p>Todo-Item</p>
+            <p>Title:title</p>
+            <p>Status:status</p>
+            <p>deadline:deadline</p> */}
+
+
+        </div>
+    )
+}
