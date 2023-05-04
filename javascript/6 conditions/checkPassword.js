7ccc54f0ca56d2d643892cb1379cfe3a30ed81a4
Sagar <dev.sagartmg@gmail.com>
2023-05-04 13:47:45 +0545

    new tasks

---
+++ javascript/6 conditions/checkPassword.js
@@ -0,0 +1,33 @@
+let user = {
+    name: "ram",
+    email: "ram@ram.com",
+    password: "ram-password"
+}
+
+
+/* OUTPUT */
+checkPassword("ram-password")  //  user logged in 
+checkPassword("ram-wrong-password")  //  Invalid Credentials
+
+
+
+/* 
+    TODO: 
+    create a function checkPassword  
+    and 
+    if the password matches for above user, print user logged in else print invalid credentials
+
+
+    as self research check for Logical operators too AND(&&) , OR(||) , NOT(!)
+    THEN , 
+    MUTATE/UPDATE THE FUNCTION  checkPassword
+
+    checkPassword("ram@ram.com","ram-password")   // user logged in 
+    checkPassword("ram@ram.com","ram-wrong-password")   // Invalid Credentails
+    checkPassword("shayam@shyam.com","ram-password")   // Invalid Credentails
+
+
+*/
+
+
+
---
+++ javascript/6 conditions/findPositivity.js
@@ -0,0 +1,20 @@
+/* Write a program that takes a number as input and checks if it is positive, negative, or zero or not a number. 
+
+
+If it is positive, output "The number is positive." 
+If it is negative, output "The number is negative." 
+If it is zero, output "The number is zero."
+If it is not a number, output "The value is not a number"
+
+
+OUTPUT:
+findPositivity(0) -> the number is zero
+findPositivity(100) -> the number is positive
+findPositivity(-1) -> the number is negative
+findPositivity("hello") -> the value is not a number 
+
+ */
+
+
+
+
