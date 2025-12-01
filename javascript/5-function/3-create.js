085b66dea2db5e60775110668022ad457a6d5b8f
dev.sagartmg <dev.sagartmg@gmail.com>
2025-12-01 23:13:48 +0545

    new taks

---
+++ javascript/5-function/3-create.js
@@ -0,0 +1,18 @@
+let users = [];
+
+addNewUser("ram", 21, "kapan");
+addNewUser("shyam", 22, "bagbazar");
+addNewUser("hari", 22, "jamal");
+
+
+console.log(users)
+/* 
+    EXPECTED-OUTPUT:
+
+    [
+        { name: 'ram', age: 21, address: 'kapan' },
+        { name: 'shyam', age: 22, address: 'bagbazar' },
+        { name: 'hari', age: 22, address: 'jamal' }
+    ]
+
+*/
---
+++ javascript/5-function/3.js
@@ -0,0 +1,34 @@
+let students = [
+  {
+    name: "Ramesh",
+    scores: [
+      { exam: "first-term", gpa: 2 },
+      { exam: "second-term", gpa: 4 }
+    ]
+  },
+  {
+    name: "Rita",
+    scores: [
+      { exam: "first-term", gpa: 1.5 },
+      { exam: "second-term", gpa: 4 }
+    ]
+  },
+  {
+    name: "Kiran",
+    scores: [
+      { exam: "first-term", gpa: 4 },
+      { exam: "second-term", gpa: 2.9 }
+    ]
+  }
+];
+
+
+/*  
+    using functon, achieve the following
+    
+    EXPECTED-OUTPUT: 
+    ramesh scored gpa 2 in first-term and 4 in second-term
+    rita scored gpa 1.5 in first-term and 4 in second-term
+    kiran scored gpa 4 in first-term and 2.9 in second-term
+
+*/
