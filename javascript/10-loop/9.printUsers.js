940f28a77f6bf0556e1a916f6c7f2e7b478b37dd
dev.sagartmg <dev.sagartmg@gmail.com>
2025-12-08 09:10:33 +0545

    new qns

---
+++ javascript/10-loop/9.printUsers.js
@@ -0,0 +1,23 @@
+let users = [
+  {
+    name: "ram",
+    subjects: ["python", "ai", "ml"],
+  },
+  {
+    name: "sita",
+    subjects: ["python", "ml"],
+  },
+  {
+    name: "hari",
+    subjects: ["js", "node"],
+  },
+];
+
+
+/* 
+    output:
+    ram is studying python, ai, ml
+    sita is studying python, ml
+    hari is studying js, node 
+
+*/
---
+++ javascript/10-loop/9.printUsersDetails.js
@@ -0,0 +1,51 @@
+let users = [
+  {
+    name: "ram",
+    subjects: [
+      {
+        name: "python",
+        price: 1000,
+      },
+      {
+        name: "ai",
+        price: 2000,
+      },
+    ],
+  },
+  {
+    name: "hari",
+    subjects: [
+      {
+        name: "python",
+        price: 1000,
+      },
+      {
+        name: "ai",
+        price: 2000,
+      },
+    ],
+  },
+  {
+    name: "sita",
+    subjects: [
+      {
+        name: "python",
+        price: 1000,
+      },
+      {
+        name: "ai",
+        price: 2000,
+      },
+      {
+        name: "ml",
+        price: 2000,
+      },
+    ],
+  },
+];
+
+/* 
+    output:
+    ram is studying python, ai, ml and his total cost is  3000
+
+*/
