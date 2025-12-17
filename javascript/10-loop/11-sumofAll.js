447cc50568d976038b2536b134608aa364200dbf
dev.sagartmg <dev.sagartmg@gmail.com>
2025-12-17 12:16:26 +0545

    css tasks

---
+++ javascript/10-loop/11-sumofAll.js
@@ -0,0 +1,13 @@
+
+
+function sum(){ //  change necessary parameters.
+
+}
+
+console.log(sum(1,2)) // 3
+console.log(sum(1,2,3)) //6
+console.log(sum(1,2,3,4)) // 10
+
+
+
+/* TODO: create a function is such a way that the function should handle all the arguements we pass to it.  */
